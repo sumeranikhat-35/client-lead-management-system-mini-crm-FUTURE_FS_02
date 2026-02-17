90673601e664721497a74d25b42993c94f4050cb
sumeranikhat-35 <sumeranikhat14@gmail.com>
2026-02-17 22:41:28 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,11 @@
+const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "sumera@4",
-  database: "mini_crm"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
@@ -12,5 +15,6 @@ db.connect((err) => {
   }
   console.log("MySQL Connected ✅");
 });
+
 
 module.exports = db;
