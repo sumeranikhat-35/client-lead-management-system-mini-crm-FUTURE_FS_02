9686e5e0f4d7b7d7aacf1d9b68c7e375c61d5a38
sumeranikhat-35 <sumeranikhat14@gmail.com>
2026-02-18 20:16:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -99,5 +99,6 @@ app.post("/login", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
+
