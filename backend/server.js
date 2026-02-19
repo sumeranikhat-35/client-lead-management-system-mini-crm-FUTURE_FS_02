9d29ff67f5c80eb7fc7f7ff93096ff538bf46607
sumeranikhat-35 <sumeranikhat14@gmail.com>
2026-02-19 13:18:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
-
-
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
@@ -132,6 +128,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
